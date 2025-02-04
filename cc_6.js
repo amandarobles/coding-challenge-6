7fe6d80867d57de5c3462601e1b673c4a5d02139
Amanda Robles <amandarobles@usf.edu>
2025-02-04 14:10:39 -0500

    Task 3 - Employee Bonus Calculation

--- cc_6.js
+++ cc_6.js
@@ -21,3 +21,20 @@ const calculateSalesTax = function(amount, taxRate) {
 //Test data
 calculateSalesTax(100, 0.07);
 calculateSalesTax(500, 0.1);
+
+// task 3: Arrow Function
+
+//Determining bonus
+const calculateBonus = (salary, performanceRating) => {
+    let bonusRate = 0;
+    if (performanceRating === "Excellent") bonusRate = 0.2;
+    else if (performanceRating === "Good") bonusRate = 0.1;
+    else if (performanceRating === "Average") bonusRate = 0.05;
+
+    let bonus = salary * bonusRate;
+    console.log(`Bonus: $${bonus}`);
+};
+
+//Test data
+calculateBonus(5000, "Excellent");
+calculateBomus(7000, "Good");
