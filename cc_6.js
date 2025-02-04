94cc8959a08b9d4569530e41452e0632496a5ecc
Amanda Robles <amandarobles@usf.edu>
2025-02-04 13:39:01 -0500

    Task 2 - Sales Tax Computation

--- cc_6.js
+++ cc_6.js
@@ -6,6 +6,18 @@ function calculateProfit(costPrice, sellingPrice, unitsSold) {
         console.log(`Total Profit: $ ${profit}`);
 }
 
-//Test cases
+//Test data
 calculateProfit(20, 30, 100);
 calculateProfit(50, 70, 200);
+
+// task 2: Function Expression
+
+//Calculating sales tax using function expression
+const calculateSalesTax = function(amount, taxRate) {
+    let tax = amount * taxRate;
+    console.log(`Sales Tax: $ ${tax}`);
+};
+
+//Test data
+calculateSalesTax(100, 0.07);
+calculateSalesTax(500, 0.1);
